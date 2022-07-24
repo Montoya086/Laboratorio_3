9ee05fab222ce962ecf2d32475f946fd7a698ce3
Montoya086 <mon21552@uvg.edu.gt>
2022-07-23 22:27:12 -0600

    Estetica

--- Lab3.kt
+++ Lab3.kt
@@ -1,10 +1,8 @@
 data class ItemData(var originalPos: Int,var originalValue: Any,var type: String? = null,var info: String? = null)
-fun main() {
-    val result = processList(listOf(25, "Hola", null, false))
-    println(result)
-}
-fun processList(inputList: List<Any?>?): List<ItemData>? {
-    var outputList: MutableList<ItemData> = mutableListOf<ItemData>()
+fun main() {val result = processList(listOf(25, "Hola", null, false))
+    println(result)}
+fun processList(inputList: List<Any?>?): List<ItemData>? { 
+    val outputList: MutableList<ItemData> = mutableListOf<ItemData>()
 	if (inputList != null){
         var cont: Int =0
         for(item: Any? in inputList){
@@ -12,9 +10,9 @@ fun processList(inputList: List<Any?>?): List<ItemData>? {
                     is String -> ItemData(cont, item, "cadena", "L"+item.length)
                     is Int -> ItemData(cont, item, "entero", if(item%10==0) "M10" else if(item%5==0) "M5" else if(item%2==0) "M2" else null)
                     is Boolean -> ItemData(cont, item, "Booleano", if (item) "Verdadero" else "Falso")
-                    else -> ItemData(cont, item, null, null)})
+                    else -> ItemData(cont, item, null, null)
+            })
             cont++
         }
     }else return null
-    return outputList
-}
+    return outputList}
